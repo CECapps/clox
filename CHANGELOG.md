9f9b82f82bf0df247139997b5793c5192c0d025a
Charles Capps <charles@capps.me>
2020-04-03 16:45:44 -0700

    14.5 Constants

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -15,3 +15,8 @@ Started using `clangd` with VSCode for code completion.  For some reason it's
 decided that all the .c files are actually C++, which is kinda weird.  There
 does not seem to be any sort of option to deal with this problem, so now my files
 are full of errors that aren't actually errors.
+
+### 14.5 Constants
+Added growable array for constant values to be tracked.
+
+Also futzed around with the definitions of NULL and size_t to shut up `clangd`.
--- src/debug.c
+++ src/debug.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 #include "debug.h"
+#include "value.h"
 
 // 14.4 Disassembling Chunks
 
@@ -10,6 +11,23 @@ void disassembleChunk(Chunk* chunk, const char* name) {
   for (int offset = 0; offset < chunk->count;) {
     offset = disassembleInstruction(chunk, offset);
   }
+}
+
+static int constantInstruction(const char* name, Chunk* chunk,
+                               int offset) {
+  uint8_t constant = chunk->code[offset + 1];
+  printf("%-16s %4d '", name, constant);
+  printValue(chunk->constants.values[constant]);
+  printf("'\n");
+
+/*
+14.5.3
+  Remember that disassembleInstruction() also returns a number to tell the caller
+  how many bytes to advance to reach the beginning of the next instruction.
+  Where OP_RETURN was only a single byte, OP_CONSTANT is two—one for the opcode
+  and one for the operand.
+*/
+  return offset + 2;
 }
 
 static int simpleInstruction(const char* name, int offset) {
@@ -22,6 +40,8 @@ int disassembleInstruction(Chunk* chunk, int offset) {
 
   uint8_t instruction = chunk->code[offset];
   switch (instruction) {
+    case OP_CONSTANT:
+      return constantInstruction("OP_CONSTANT", chunk, offset);
     case OP_RETURN:
       return simpleInstruction("OP_RETURN", offset);
     default:
