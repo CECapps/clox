c110962e4dab25a78f0fb2b6ccf26a835080477b
Charles Capps <charles@capps.me>
2020-04-24 10:34:44 -0700

    22.2 Block Statements

--- src/compiler.c
+++ src/compiler.c
@@ -171,6 +171,16 @@ static void endCompiler() {
 }
 
 
+static void beginScope() {
+  current->scopeDepth++;
+}
+
+
+static void endScope() {
+  current->scopeDepth--;
+}
+
+
 static void initCompiler(Compiler* compiler) {
   compiler->localCount = 0;
   compiler->scopeDepth = 0;
@@ -378,6 +388,15 @@ static void expression() {
 }
 
 
+static void block() {
+  while (!check(TOKEN_RIGHT_BRACE) && !check(TOKEN_EOF)) {
+    declaration();
+  }
+
+  consume(TOKEN_RIGHT_BRACE, "Expect '}' after block.");
+}
+
+
 static void varDeclaration() {
   uint8_t global = parseVariable("Expect variable name.");
 
@@ -447,6 +466,10 @@ static void declaration() {
 static void statement() {
   if (match(TOKEN_PRINT)) {
     printStatement();
+  } else if (match(TOKEN_LEFT_BRACE)) {
+    beginScope();
+    block();
+    endScope();
   } else {
     expressionStatement();
   }
